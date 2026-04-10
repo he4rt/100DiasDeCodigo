a4b571eb108254f864719e43503ddb6678542e84
Gabriel do Carmo Vieira <48625433+gvieira18@users.noreply.github.com>
2026-04-09 21:55:07 -0300

    feat(backup): add SFTP configuration and cleanup strategy

--- routes/console.php
+++ routes/console.php
@@ -4,4 +4,19 @@ declare(strict_types=1);
 
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::command('cloudflare:reload')->daily();
+Schedule::command('cloudflare:reload')
+    ->when(fn () => config('laravelcloudflare.enabled'))
+    ->twiceMonthly(3, 17);
+
+Schedule::command('backup:run', ['--only-db'])
+    ->when(fn () => config('backup.enabled'))
+    ->everyFifteenMinutes();
+
+Schedule::command('backup:clean')
+    ->when(fn () => config('backup.enabled'))
+    ->everyOddHour();
+
+Schedule::command('backup:monitor')
+    ->when(fn () => config('backup.enabled'))
+    ->daily()
+    ->at('09:00');
